4c758cdc0d6a60db4a9b8179fa5d2bf05b239197
GameLogist <36932180+GameLogist@users.noreply.github.com>
2019-01-16 00:28:34 +0530

    Add files via upload

---
+++ Association Rule Learning/Apriori/apriori_test.R
@@ -0,0 +1,8 @@
+dataset = read.csv('Market_Basket_Optimisation.csv', header=FALSE)
+library(arules)
+dataset = read.transactions('Market_Basket_Optimisation.csv', sep=',', rm.duplicates = TRUE)
+summary(dataset)
+itemFrequencyPlot(dataset, topN = 100)
+
+rules = apriori(data = dataset, parameter = list(support = 0.004,confidence = 0.2))
+inspect(sort(rules, by='lift')[1:10])
