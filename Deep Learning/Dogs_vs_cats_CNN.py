ad0ee4a3c265930eecb811a563a295d704bc024c
Prashant Dixit <32432618+prashant564@users.noreply.github.com>
2019-10-24 01:40:36 +0530

    Update Dogs_vs_cats_CNN.py

--- Deep Learning/Dogs_vs_cats_CNN.py
+++ Deep Learning/Dogs_vs_cats_CNN.py
@@ -97,7 +97,6 @@ def prepare(filepath):
     image_array=cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
     new_img = cv2.resize(image_array, (IMG_SIZE, IMG_SIZE))
     return new_img.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-
 prediction=model.predict([prepare('D:\250px-Gatto_europeo4.jpg')])
 print(predict[int(prediction[0][0])])
    
